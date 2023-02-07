022c418bf276e8067da68ff7d31f79cbc609dd4b
DiegoRos3 <124701925+DiegoRos3@users.noreply.github.com>
2023-02-07 09:50:03 -0600

    Add files via upload

---
+++ EVA1_1_HOLAMUNDO/src/eva1_1_holamundo/EVA1_1_HOLAMUNDO.java
@@ -0,0 +1,11 @@
+package eva1_1_holamundo;
+
+public class EVA1_1_HOLAMUNDO {
+
+    public static void main(String[] args) {
+        // Practica 1, hola mundo
+        System.out.println("Hola Mundo!");
+        
+    }
+    
+}
