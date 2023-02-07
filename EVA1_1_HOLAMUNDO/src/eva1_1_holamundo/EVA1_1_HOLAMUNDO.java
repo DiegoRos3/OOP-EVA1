575490eabda3fab8880e8f8b4456dc4c47fe616f
Diego Rose <124701925+DiegoRos3@users.noreply.github.com>
2023-02-07 16:35:54 -0600

    Update EVA1_1_HOLAMUNDO.java

--- EVA1_1_HOLAMUNDO/src/eva1_1_holamundo/EVA1_1_HOLAMUNDO.java
+++ EVA1_1_HOLAMUNDO/src/eva1_1_holamundo/EVA1_1_HOLAMUNDO.java
@@ -3,7 +3,13 @@ package eva1_1_holamundo;
 public class EVA1_1_HOLAMUNDO {
 
     public static void main(String[] args) {
-        // Practica 1, hola mundo
+        /* 
+           Practica 1: Hola mundo
+           Nombre: Diego Aarón Rose Aguirre
+           # control: 22550309 
+           07/02/2023 
+        */
+        
         System.out.println("Hola Mundo!");
         
     }
