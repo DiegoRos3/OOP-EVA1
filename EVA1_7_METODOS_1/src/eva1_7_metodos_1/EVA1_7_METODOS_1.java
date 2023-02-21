03b37cce88818ee4aa76ce8e6fc68e85b1fe0647
Diego Rose <124701925+DiegoRos3@users.noreply.github.com>
2023-02-21 16:29:06 -0600

    Add files via upload

---
+++ EVA1_7_METODOS_1/src/eva1_7_metodos_1/EVA1_7_METODOS_1.java
@@ -0,0 +1,13 @@
+package eva1_7_metodos_1;
+
+public class EVA1_7_METODOS_1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        myMethod();
+    }
+    
+    public static void myMethod(){
+        System.out.println("You have called me: my name is myMethod!!");
+    }
+}
