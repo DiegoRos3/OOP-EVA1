11756c6d6d6b745f14d51195b8aa8b771970f022
Diego Rose <124701925+DiegoRos3@users.noreply.github.com>
2023-02-24 08:58:53 -0600

    Add files via upload

---
+++ EVA1_15_SOBRECARGA/src/eva1_15_sobrecarga/EVA1_15_SOBRECARGA.java
@@ -0,0 +1,17 @@
+package eva1_15_sobrecarga;
+
+public class EVA1_15_SOBRECARGA {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        /*DIFERENTES PRINTLN: SON 10 DIFERENTES METODOS, PARA IMPRIMIR
+        DIFERENTES TIPOS DE DATOS*/
+        /* PARA CREAR SOBRE CARGA DE METODOS: LA FIRMA DEBE DE SER UNICA*/
+        System.out.println("Hola mundo");
+        System.out.println(100);
+        System.out.println(true);
+        System.out.println(args);
+    }
+    
+}
